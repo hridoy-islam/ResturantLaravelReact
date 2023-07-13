87be44497f7f2a07bce52db9f1b54de382397845
Hridoy Islam <hridoy4t@gmail.com>
2023-07-13 16:50:41 +0600

    scroll

--- react/src/Components/Layouts/Main.jsx
+++ react/src/Components/Layouts/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Shared/Footer";
 import Header from "../Shared/Header";
+// import ScrollToTop from "../ScrollToTop";
 
 const Main = () => {
     return (
         <div>
             <Header />
+            
             <div className="pt-14">
                 <Outlet></Outlet></div>
             <Footer />
---
+++ react/src/Components/ScrollToTop/ScrollToTop.jsx
@@ -0,0 +1,38 @@
+// import { FaAngleUp } from "react-icons/fa";
+
+import { useEffect, useState } from "react";
+import "./ScrollTTop.css"
+
+const ScrollToTop = () => {
+    const [showTopBtn, setShowTopBtn] = useState(false);
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 400) {
+                setShowTopBtn(true);
+            } else {
+                setShowTopBtn(false);
+            }
+        });
+    }, []);
+    const goToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
+    return (
+        <div className="top-to-btm">
+            {" "}
+            {showTopBtn && (
+                <span className=" bg-primary icon-position icon-style" onClick={goToTop}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 pt-3 mx-auto">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />
+</svg>
+
+              </span>
+                
+            )}{" "}
+        </div>
+    );
+};
+export default ScrollToTop;
