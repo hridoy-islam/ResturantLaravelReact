66736c221a9247839558e6493c1c52700a36c5ad
mushfik16381 <ahmed.mushfik16381@gmail.com>
2023-09-13 23:03:20 +0600

    search date

--- react/src/Pages/Admin/Order.jsx
+++ react/src/Pages/Admin/Order.jsx
@@ -1,17 +1,18 @@
 
-import axios from 'axios';
+// import axios from 'axios';
 import PageTitle from '../../Components/Shared/PageTitle';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import DatePicker from "react-datepicker";
+// import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
-import moment from 'moment/moment';
+import { data } from 'autoprefixer';
+// import moment from 'moment/moment';
 // import OrderView from './OrderView';
 const Order = () => {
-    const [startDate, setStartDate] = useState(new Date());
+    // const [startDate, setStartDate] = useState(new Date());
     let [search, setSearch] = useState("");
-    var today = moment().format('YYYY-MM-DD');
+    // var today = moment().format('YYYY-MM-DD');
     // const [orders, setOrders] = useState([]);
     // const fetchData = () => {
     //     axios.get(`${import.meta.env.VITE_BACKEND_API_URL}/order/`)
@@ -45,7 +46,7 @@ const Order = () => {
             .then((res) => res.json())
             .then((data) => setOrders(data));
     }, [orderStatus, setOrderStatus]);
- console.log(orders?.date)
+    console.log(data)
     return (
         <div>
             <PageTitle title="Order" />
@@ -71,16 +72,23 @@ const Order = () => {
                             Completed order
                         </option>
                     </select>
+                    <div className="form-control search-order">
+                        <input
+                            type="text"
+                            placeholder="Search Create Date"
+                            className="input input-bordered w-full"
+                            onChange={(e) => setSearch(e.target.value)}
+                        />
+                    </div>
+                    {/* <div onClick={(e) => setOrderStatus(e.target.value)}>
+                        <DatePicker value={`${import.meta.env.VITE_BACKEND_API_URL}/order/datequery/?createdAt=2023-08-31`} selected={startDate} onChange={(date) => setStartDate(date)} />
+                        <button type="submit" >Submit</button>
+                    </div> */}
 
-                    <div onClick={(e) => setOrderStatus(e.target.value)}>
-                    <DatePicker value={`${import.meta.env.VITE_BACKEND_API_URL}/order/datequery/?createdAt=2023-08-31`} selected={startDate} onChange={(date) => setStartDate(date)} />
-                    <button type="submit" >Submit</button>
-                        </div>
-
-                    <form   onClick={(e) => setOrderStatus(e.target.value)}>
-                        <input  type="date" name="date" id="date"  className="px-2 py-3 rounded-lg mr-2 bg-gray-100 border border-black" />
+                    {/* <form onClick={(e) => setOrderStatus(e.target.value)}>
+                        <input type="date" name="date" id="date" className="px-2 py-3 rounded-lg mr-2 bg-gray-100 border border-black" />
                         <button type="submit" value={`${import.meta.env.VITE_BACKEND_API_URL}/order/datequery/?date=2023-09-15`}>Submit</button>
-                    </form>
+                    </form> */}
                     {/* <button className="btn btn-outline font-semibold px-3">Apply Filter</button> */}
                 </div>
             </div>
@@ -91,35 +99,36 @@ const Order = () => {
                             <th className='text-lg font-bold'>Email</th>
                             <th className='text-lg font-bold'>Meal Plan</th>
                             <th className='text-lg font-bold'>Status</th>
-                            <th className='text-lg font-bold'>Date</th>
+                            <th className='text-lg font-bold'>Create Date</th>
                             <th className='text-lg font-bold'>Price</th>
                             <th className='text-lg font-bold'>Action</th>
                         </tr>
                     </thead>
-                        
+
                     <tbody>
-                    {orders.filter((data) => {
-                                return search.toLocaleLowerCase() === ""
-                                    ? data
-                                    : data.toLocaleLowerCase().includes(search);
-                            })
+                        {orders.filter((item) => {
+                            console.log(item?.plan)
+                            return search.toLocaleLowerCase() === ""
+                                ? item
+                                : item?.createdAt.toLocaleLowerCase().includes(search);
+                        })
                             .map((item, index) => <tr className='my-2' key={index}>
                                 <td className='text-lg font-normal'>{item.email}</td>
                                 <td className='text-lg font-normal'>{item.plan}</td>
                                 <td className='text-lg font-normal'>{item.orderStatus}</td>
-                                <td className='text-lg font-normal'>{item.createdAt}</td>
+                                <td className='text-lg font-normal'>{item.createdAt.slice(0,10)}</td>
                                 <td className='text-lg font-normal'>{item.price} AED</td>
                                 <td> <Link to={`/admin/order/${item._id}`} className='text-white bg-primary px-3 py-2'>view</Link></td>
                             </tr>)}
                     </tbody>
                 </table>
-                
+
                 <div className="join mt-12 flex justify-center">
-                            <button className="join-item btn btn-active">1</button>
-                            <button className="join-item btn ">2</button>
-                            <button className="join-item btn">3</button>
-                            <button className="join-item btn">4</button>
-                        </div>
+                    <button className="join-item btn btn-active">1</button>
+                    <button className="join-item btn ">2</button>
+                    <button className="join-item btn">3</button>
+                    <button className="join-item btn">4</button>
+                </div>
             </div>
         </div>
     );
